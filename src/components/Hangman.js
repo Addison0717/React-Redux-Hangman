b6e35ef6fc5193e03abe88b8e6aea54335747101
Addison Schultz <addisonschultz0@gmail.com>
2018-06-17 12:42:49 +0200

    added final word to loss screen

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -11,14 +11,24 @@ export default function Hangman(props) {
             <hr/>
             <br/>
             <h3>Wrong Guess Count: { gameLogic.wrongGuessCount(props.word, props.guesses) }</h3>
-            <h3> { gameLogic.showGuess(props.word, props.guesses) } is your secret word! </h3>
+            <h3> { gameLogic.showGuess(props.word, props.guesses) } </h3>
             <h3> Your Guessed Letters: { props.guesses } </h3>
           </div>
         )
       }  else if (gameLogic.isWinner(props.word, props.guesses) ) {
-        return (<h1>You won!!!</h1>)
+        return (
+          <div>
+          <h1>You won!!!</h1>
+          <h3> { gameLogic.showGuess(props.word, props.guesses) } </h3>
+          </div>
+        )
       } else {
-        return (<h2>Game over, you lost :( </h2>)
+        return (
+          <div>
+          <h2>Game over, you lost :( </h2>
+          <h3> { props.word } was the answer! </h3>
+          </div>
+          )
       }
   }
 
